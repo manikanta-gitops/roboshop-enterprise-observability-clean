41221b3a20d7e63e50e1e4833c5444bcc754881e
Roboshop Technologies <dineshmallapuram1@gmail.com>
2026-08-13 13:18:45 +0530

    Update backend.tf

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "roboshop-tfstate-347076821255"
     key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
